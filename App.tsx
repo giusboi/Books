28889101b9b60a619f549d242c918a0cb68f3efb
Giuseppe Boi <giuseppe.boi@synesthesia.it>
2022-01-25 10:24:02 +0100

    minor.

--- App.tsx
+++ App.tsx
@@ -1,8 +1,5 @@
 import React from 'react';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { AppNavigator } from './src/Navigation';
-
-const MainStack = createNativeStackNavigator()
 
 export default function App() {
   return (
