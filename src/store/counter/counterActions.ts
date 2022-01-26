708862f51eae065fe842ed615975293c6d353ab1
Giuseppe Boi <giuseppe.boi@synesthesia.it>
2022-01-26 13:50:52 +0100

    minor.

--- src/store/counter/counterActions.ts
+++ src/store/counter/counterActions.ts
@@ -21,13 +21,12 @@ export function incrementAfter(
   milliseconds: number
 ): AppThunkAction {
   return async (dispatch, getState) => {
-    if (getState().counter.count < 10) {
+    if (getState().counter.count < from) {
       dispatch(increment())
       return
     }
     setTimeout(() => {
       dispatch(increment())
-      console.log(getState().counter.count)
     }, milliseconds)
   }
 }
