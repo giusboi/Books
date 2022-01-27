85ba2ddff8e4ab5ea4bea8540b785508e57cc392
Giuseppe Boi <giuseppe.boi@synesthesia.it>
2022-01-27 13:02:33 +0100

    Api done, added flatLists. Get books done. Stable. Take this commit to take a look to network client and how to handle a distributed state without redux.

--- src/screens/DetailScreen.tsx
+++ src/screens/DetailScreen.tsx
@@ -1,7 +1,9 @@
-import React, { useEffect } from 'react';
-import { Text, View } from 'react-native';
+import React, { useCallback, useEffect, useState } from 'react';
+import { FlatList, ListRenderItem, Text, View } from 'react-native';
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import { StackParamList } from '../Navigation';
+import { ApiClient } from '../managers/api/ApiClient';
+import { Book } from '../managers/api/models/Book';
 
 type NavProps = NativeStackScreenProps<StackParamList, 'Detail'>
 
@@ -10,10 +12,33 @@ interface Props extends NavProps {}
 export const DetailScreen = (props: Props) => {
   const { route } = props
   const { listNameEncoded } = route.params
+  const [books, setBooks] = useState<ReadonlyArray<Book>>([])
+
+  useEffect(() => {
+    if (!listNameEncoded) {
+      return
+    }
+    ApiClient.getBooks(listNameEncoded).then(setBooks)
+  }, [listNameEncoded])
+
+  const renderItem = useCallback<ListRenderItem<Book>>((book) => {
+    return (
+      <View>
+        <Text>{book.item.title}</Text>
+      </View>
+    )
+  }, [])
+
+  const keyExtractor = useCallback((item, index) => `book_${index}`, [])
 
   return (
-    <View>
+    <View style={{flex: 1}}>
       <Text>The listNameEncoded {listNameEncoded}</Text>
+      <FlatList
+        data={books}
+        renderItem={renderItem}
+        keyExtractor={keyExtractor}
+      />
     </View>
   )
 }
